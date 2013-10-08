a645c2babe6a25584ad15c5b023afd0bc67bb6d1
Leo Gallucci <elgalu3@gmail.com>
2013-10-07 22:53:31 -0300

    page 30

---
+++ mail_form/test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dummy::Application.config.secret_key_base = '1f1c689e3abe57f7038af55fa6982eb4b0c941bdfdfba5401ad147ac24b2863dc0df685cf6425835368e95601f26a3bc76254348d2d968bebe82d024e0e93eea'
---
+++ mail_form/test/mail_form_test.rb
@@ -0,0 +1,12 @@
+require 'test_helper'
+require 'fixtures/sample_mail'
+
+class MailFromTest < ActiveSupport::TestCase
+  test 'sample mail has name and email as attributes' do
+    sample = SampleMail.new
+    sample.name = 'User'
+    assert_equal 'User', sample.name
+    sample.email = 'user@example.com'
+    assert_equal 'user@example.com', sample.email
+  end
+end
