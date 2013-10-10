52b2ee6914445719b0d449ea606383fcd5959a60
Leo Gallucci <elgalu3@gmail.com>
2013-10-10 11:22:57 -0300

    Capybara. page 43

--- mail_form/test/dummy/config/routes.rb
+++ mail_form/test/dummy/config/routes.rb
@@ -1,4 +1,7 @@
 Dummy::Application.routes.draw do
+  resources :contact_forms, only: :create
+  root to: 'contact_forms#new'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -39,7 +42,7 @@ Dummy::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
