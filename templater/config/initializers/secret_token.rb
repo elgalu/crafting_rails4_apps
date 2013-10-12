fa00b3384cb3212150cf338ee42c20e00d68a000
Leo Gallucci <elgalu3@gmail.com>
2013-10-12 15:48:59 -0300

    rails new templater. page 57

---
+++ templater/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Templater::Application.config.secret_key_base = '0047fc5cbf9c7f8934525cf6690bd7221b3f7e96ef77e1bc288d04e62670483a914721784df5ceec785ee6897b11e85022fe471dc80328097810261182d689ea'
