ba20ceaa20396858c37bdadad7f86be4dfe2534b
Leo Gallucci <elgalu3@gmail.com>
2013-10-21 16:55:46 -0300

    Add live_assets at page 95

---
+++ live_assets/test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dummy::Application.config.secret_key_base = '674d8d6a885dc78727a2f9ef1c98097f23e9509d2823d263443bd62c5abb5dc1a7878c17ed019d7b4b2a79fdab27d88fd9b87793d704737a2c2a73d3a9822750'
