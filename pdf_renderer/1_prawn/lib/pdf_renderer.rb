bae5a30db2bdb976bdf90a55d108732300296cb3
Leo Gallucci <elgalu3@gmail.com>
2013-10-27 12:31:03 -0300

    styling

--- pdf_renderer/1_prawn/lib/pdf_renderer.rb
+++ pdf_renderer/1_prawn/lib/pdf_renderer.rb
@@ -5,4 +5,3 @@ ActionController::Renderers.add :pdf do |filename, options|
   pdf.text render_to_string(options)
   send_data(pdf.render, filename: "#{filename}.pdf", disposition: 'attachment')
 end
-
