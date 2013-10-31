77c9f88531f58f51f75ac95295524da6bf6899d1
Leo Gallucci <elgalu3@gmail.com>
2013-10-31 16:28:03 -0300

    Trying to avoid concurrency in order to make the tests pass

--- live_assets/test/dummy/config/environments/production.rb
+++ live_assets/test/dummy/config/environments/production.rb
@@ -77,4 +77,6 @@ Dummy::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.allow_concurrency = true
 end
