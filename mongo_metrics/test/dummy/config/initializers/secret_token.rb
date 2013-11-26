76e5f174f72036c58bcee7e880693779adf70594
Leo Gallucci <elgalu3@gmail.com>
2013-11-26 18:00:16 -0300

    mongo_metris; page 146

---
+++ mongo_metrics/test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dummy::Application.config.secret_key_base = '875696c6100d3009b01dfe820df9934a3c6af04a82ce670dc9c8359c1e0fa370833d44eb335951be6925ede9ce0d6fc33e997f8d9f6bbd6c9d0a237dbfe5b6b5'
