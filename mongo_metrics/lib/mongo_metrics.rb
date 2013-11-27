0b8999bdaa622febdec299d7c604b734a8d63fe1
Leo Gallucci <elgalu3@gmail.com>
2013-11-27 15:46:21 -0300

    mongo_metrics; page 152

--- mongo_metrics/lib/mongo_metrics.rb
+++ mongo_metrics/lib/mongo_metrics.rb
@@ -1,6 +1,7 @@
-require "mongoid"
-require "mongo_metrics/engine"
-require "active_support/notifications"
+require 'mongoid'
+require 'mongo_metrics/engine'
+require 'active_support/notifications'
+require 'jquery-rails' # to fix https://github.com/rails/rails/commit/007f5670
 
 module MongoMetrics
   EVENT = 'process_action.action_controller'
--- mongo_metrics/mongo_metrics.gemspec
+++ mongo_metrics/mongo_metrics.gemspec
@@ -17,4 +17,5 @@ Gem::Specification.new do |s|
   s.test_files = Dir["test/**/*"]
 
   s.add_runtime_dependency "rails", "~> 4.0.1"
+  s.add_runtime_dependency "jquery-rails", "~> 3.0.4"
 end
